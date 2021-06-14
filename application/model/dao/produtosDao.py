3591f15981dfab8e960626bd444a2173b5a95a70
Daniel Cabral <danielccabral1981@gmail.com>
2021-06-14 20:19:05 -0300

    Iniciando Dao

---
+++ application/model/dao/produtosDao.py
@@ -0,0 +1,17 @@
+
+
+from application.model.entity.produtos import Produtos
+import json
+
+class ProdutoDao:
+    def __init__(self):
+        self._listaProdutos=[]
+
+        with open('C:\\Users\\DKs\\Documents\\Projetos_GitHub\\DesafioBuscape\\application\\view\\static\\json\\produtos.json') as diretorio_produto:
+            lista_produtos = json.load(diretorio_produto)
+
+        for i in lista_produtos:
+            self._listaProdutos.append(Produtos(i['id'], i['name'], i['images'], i['price']['value'], i['price']['installments'], i['price']['installmentValue']))
+
+    def get_exibir_listaProdutos(self):
+        return self._listaProdutos
