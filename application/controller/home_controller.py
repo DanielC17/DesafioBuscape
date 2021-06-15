514d93ed5ec653b0f4f9281d7212b41d329af79d
Daniel Cabral <danielccabral1981@gmail.com>
2021-06-15 15:56:03 -0300

    adicionado as app.routes de inserir e excluir

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -2,10 +2,28 @@ import json
 from application import app
 from application.model.dao.produtosDao import ProdutoDao
 from flask import render_template, request, url_for, redirect
-from application.model.entity.produtos import Produtos
+
+produtosDao = ProdutoDao()
+lista_carrinho = produtosDao.lista_carrinho()
+lista_produtos = produtosDao.get_exibir_listaProdutos()
 
 
 @app.route("/")
-def home():
-    lista_produtos = ProdutoDao().get_exibir_listaProdutos()
-    return render_template("index.html", lista_produtos = lista_produtos)
+def index():
+    return render_template("index.html", lista_produtos = lista_produtos, lista_carrinho = lista_carrinho)
+
+
+@app.route("/inserir/<id>")
+def inserir(id):
+    for i in lista_produtos:
+        if int(id) == i.get_id():
+            lista_carrinho.append(i)
+            return redirect(url_for('index'))
+
+
+@app.route("/excluir/<id>")
+def excluir(id):
+    for i in lista_carrinho:
+        if int(id) == i.get_id():
+            lista_carrinho.remove(i)
+            return redirect(url_for('index'))
