ccbf632234694198ba5f77eebc306083310db4ec
Daniel Cabral <danielccabral1981@gmail.com>
2021-06-15 15:55:33 -0300

    setando as listas para serem exibidas no index

--- application/model/dao/produtosDao.py
+++ application/model/dao/produtosDao.py
@@ -3,9 +3,11 @@
 from application.model.entity.produtos import Produtos
 import json
 
+
 class ProdutoDao:
     def __init__(self):
-        self._listaProdutos=[]
+        self._carrinhoList = []
+        self._listaProdutos = []
 
         with open('C:\\Users\\DKs\\Documents\\Projetos_GitHub\\DesafioBuscape\\application\\view\\static\\json\\produtos.json') as diretorio_produto:
             lista_produtos = json.load(diretorio_produto)
@@ -13,5 +15,11 @@ class ProdutoDao:
         for i in lista_produtos:
             self._listaProdutos.append(Produtos(i['id'], i['name'], i['images'], i['price']['value'], i['price']['installments'], i['price']['installmentValue']))
 
+    def lista_carrinho(self):
+        return self._carrinhoList
+
     def get_exibir_listaProdutos(self):
         return self._listaProdutos
+
+
+
